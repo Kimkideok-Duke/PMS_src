66ecdc64273878dc9478f14ed08869b6aaeb5c4b
hopels <101801438+hopels@users.noreply.github.com>
2022-07-28 14:37:42 +0900

    Update Account.sql

--- database/Account.sql
+++ database/Account.sql
@@ -2,6 +2,13 @@
 계정 테이블 생성
 사원번호 (userno) 기본키
 권한번호 (authno) 외래키 (권한테이블)
+
+권한(auth)
+master (전체 관리자)
+pm (프로젝트 관리자)
+um (유저 관리자)
+user (일반 사용자)
+
 */
 CREATE TABLE account(
 	userno varchar2(9) PRIMARY KEY,
@@ -13,28 +20,10 @@ CREATE TABLE account(
 	hpnum varchar2(15),
 	birthdate DATE,
 	joindate DATE NOT NULL,
-	authno NUMBER REFERENCES auth(authno)
+	auth varchar2(30) CHECK(auth IN('master','pm','um','user'))
 );
-/*
-권한테이블 생성
-권한번호 (authno) 기본키
- */
-CREATE TABLE auth(
-	authno NUMBER PRIMARY KEY,
-	auth varchar2(30)
-);
-/*
-권한테이블 데이터
-master (전체 관리자)
-pm (프로젝트 관리자)
-um (유저 관리자)
-user (일반 사용자)
- */
 
-INSERT INTO auth values(10, 'master');
-INSERT INTO auth values(20, 'pm');
-INSERT INTO auth values(30, 'um');
-INSERT INTO auth values(40, 'user');
+
 
 /*
 사원번호(userno) 시퀀스 생성 / 숫자8자리
@@ -48,10 +37,9 @@ CREATE SEQUENCE acc_seq
 
 
 INSERT INTO account values('E'||to_char(acc_seq.nextval),'abcabc123','홍길동','himan7777@naver.com',
-	'과장','서울시 마포구 서교동 447-5 201호','010-1234-5678',NULL,sysdate,10);
+	'과장','서울시 마포구 서교동 447-5 201호','010-1234-5678',NULL,sysdate,'master');
 
 SELECT * FROM account;
-SELECT * FROM auth;
 
 
 
